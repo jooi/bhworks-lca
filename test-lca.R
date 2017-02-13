a6cfa5deaa668cfe6716ffaaae5f4b404acd1e7b
jooi <gavyn@jhu.edu>
2017-02-13 13:52:31 -0500

    completed first LCA and transformations

--- test-lca.R
+++ test-lca.R
@@ -1,8 +1,18 @@
 # set up environment
-setwd("C:/Users/Gavyn/Documents/R/BH-Works/LCA/bhworks-lca")
+# setwd("C:/Users/Gavyn/Documents/R/BH-Works/LCA/bhworks-lca")
 library(poLCA)
-#test command
+
+#define function
+f <- cbind(BHSED02, BHSED04, BHSED05, BHSED06, BHSED07)~1
 
 # load the data
-attr <- read.table('1023.csv', header=TRUE, sep=',', stringsAsFactors = FALSE)
+attr <- read.csv('1023.csv', header=TRUE, sep=',', stringsAsFactors = FALSE)
 
+# convert all integers into positive integers
+attr2 <- subset(attr, Form.Version == "PC 12 to 24", select=c(BHSED02, BHSED04, BHSED05, BHSED06, BHSED07))
+
+# transform data to positive
+attr3 <- abs(attr2)
+
+# run LCA
+test <- poLCA(f, attr3, nclass=2)
