74666e867d809463304be8c8de1fffa9ae289331
Eric <li229@yahoo.com>
2017-02-13 11:10:42 -0500

    test

--- test-lca.R
+++ test-lca.R
@@ -1,6 +1,7 @@
 # set up environment
 setwd("C:/Users/Gavyn/Documents/R/BH-Works/LCA/bhworks-lca")
 library(poLCA)
+#test command
 
 # load the data
 attr <- read.table('1023.csv', header=TRUE, sep=',', stringsAsFactors = FALSE)
