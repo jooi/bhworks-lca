02333856c9e02577d32030a61c069d2ed64224ad
jooi <gavyn@jhu.edu>
2017-02-13 10:53:42 -0500

    adding initial files

---
+++ test-lca.R
@@ -0,0 +1,7 @@
+# set up the environment
+setwd("C:/Users/Gavyn/Documents/R/BH-Works/LCA")
+library(poLCA)
+
+# load the data
+attr <- read.table('1023.csv', header=TRUE, sep=',', stringsAsFactors = FALSE)
+
