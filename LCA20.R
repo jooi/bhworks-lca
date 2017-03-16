977d75e4a8b4178fc363301f98d89f4380c1e6fe
Eric <li229@yahoo.com>
2017-03-15 23:05:29 -0400

    partial data selection logic fixed

--- LCA20.R
+++ LCA20.R
@@ -2,11 +2,17 @@
 dataset = read.csv('joanna_raw.csv',
                    header=TRUE)
 
+#redefine the variables value based on the form logic
+dataset$BHSSA02A = ifelse (dataset$BHSSA02 == 0, 0, dataset$BHSSA02A)
+dataset$BHSSA03A = ifelse (dataset$BHSSA03 == 0, 0, dataset$BHSSA03A)
+
 # selecting 20 variables from the PC data
 dataset1 <- subset(dataset, 
                    select=c(Gender, BHSSA02A, BHSSA03A, BHSSC02, BHSSC03, BHSF02, BHSF05, 
                            BHSSF02, BHSSF04, BHSSX01, BHSSX06, BHSSX05, BHSSA05,
                            BHSSA06, BHSSA07, BHSSA08, BHST01, BHST02, BHST03, BHST04))
+
+
 
 # omitting the missing data
 dataset2 <- na.omit(dataset1)
@@ -23,17 +29,10 @@ dataset2$BHSF05[BHSF05<5] <- 0
 dataset2$BHSF05[BHSF05==5] <- 1
 dataset2$BHSSF02[BHSSF02>0] <- 1
 dataset2$BHSSF04[BHSSF04>0] <- 1
-if ((Gender == 1) && (BHSSX01 !=2)) {
-  dataset2$BHSSX01 <- 1
-}  else if ((Gender ==2) && (BHSSX01 != 1)) {
-  dataset2$BHSSX01 <- 1
-}  else {dataset2$BHSSX01 <- 0}
-
-if ((Gender == 1) && (BHSSX06 !=3)) {
-  dataset2$BHSSX06 <- 1
-}  else if ((Gender ==2) && (BHSSX06 != 4)) {
-  dataset2$BHSSX06 <- 1
-}  else {dataset2$BHSSX06 <- 0}
+dataset2$BHSSX01 = ifelse (Gender == 0, ifelse(BHSSX01 !=1, 1, 0), 
+                           ifelse(BHSSX01 !=2, 1, 0))
+dataset2$BHSSX06 = ifelse (Gender == 0, ifelse(BHSSX06 !=4, 1, 0), 
+                           ifelse(BHSSX06 !=3, 1, 0))
 
 dataset2$BHSSX05[BHSSX05>0] <- 1
 dataset2$BHSSA05[BHSSA05>0] <- 1
@@ -66,12 +65,12 @@ dataset3 <- as.data.frame(lapply(dataset3, as.factor))
 dataset3$LCA.Class <- factor(as.character(LCAmodel$predclass))
 
 # conduct logistic regression
-# model <- glm(LCA.Class ~ BHSSA02A+BHSSA03A+BHSSC02+BHSSCFail+BHSF02+BHSF05+
-#              BHSSF02+BHSSF04
-#             # +BHSSX01+BHSSX06 # These two varibales are all zero
-#              +BHSSX05+BHSSA05+
-#              BHSSA06+BHSSA07+BHSSA08+BHST01+BHST02+BHST03+BHST04, data=dataset3, family="binomial")
-# summary(model)
+model <- glm(LCA.Class ~ BHSSA02A+BHSSA03A+BHSSC02+BHSSCFail+BHSF02+BHSF05+
+              BHSSF02+BHSSF04
+              +BHSSX01+BHSSX06
+              +BHSSX05+BHSSA05+
+              BHSSA06+BHSSA07+BHSSA08+BHST01+BHST02+BHST03+BHST04, data=dataset3, family="binomial")
+ summary(model)
 
 # this model includes the top 7 from the paper; we have more managable beta coefficients here
 # when we lower the number of predictors in the logistic regression model
