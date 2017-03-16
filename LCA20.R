5c2027f755d7a00b11e137e61f582e552b875254
Eric <li229@yahoo.com>
2017-03-15 23:18:33 -0400

    step number

--- LCA20.R
+++ LCA20.R
@@ -1,23 +1,21 @@
-# load the data
+#1 load the data
 dataset = read.csv('joanna_raw.csv',
                    header=TRUE)
 
-#redefine the variables value based on the form logic
+#2 redefine the variables value based on the form logic
 dataset$BHSSA02A = ifelse (dataset$BHSSA02 == 0, 0, dataset$BHSSA02A)
 dataset$BHSSA03A = ifelse (dataset$BHSSA03 == 0, 0, dataset$BHSSA03A)
 
-# selecting 20 variables from the PC data
+#3 selecting 20 variables from the PC data
 dataset1 <- subset(dataset, 
                    select=c(Gender, BHSSA02A, BHSSA03A, BHSSC02, BHSSC03, BHSF02, BHSF05, 
                            BHSSF02, BHSSF04, BHSSX01, BHSSX06, BHSSX05, BHSSA05,
                            BHSSA06, BHSSA07, BHSSA08, BHST01, BHST02, BHST03, BHST04))
 
-
-
-# omitting the missing data
+#4 omitting the missing data
 dataset2 <- na.omit(dataset1)
 
-# dichotomize the variables
+#5 dichotomize the variables
 attach(dataset2)
 dataset2$BHSSA02A[BHSSA02A>0] <- 1
 dataset2$BHSSA03A[BHSSA03A>0] <- 1
@@ -46,31 +44,31 @@ dataset2$BHST04[BHST04>0] <- 1
 
 detach(dataset2)
 
-# dichotomize the variables
+#6 redefine variable values from 0/1 to 1/2
 dataset3 <- apply(dataset2, 2, function(x) ifelse(x > 0, 2, 1))
 dataset3 <- as.data.frame(dataset3)
 
-# define function
+#7 define function
 f <- cbind(BHSSA02A, BHSSA03A, BHSSC02, BHSSCFail, BHSF02, BHSF05, 
            BHSSF02, BHSSF04, BHSSX01, BHSSX06, BHSSX05, BHSSA05,
            BHSSA06, BHSSA07, BHSSA08, BHST01, BHST02, BHST03, BHST04)~1
 
-# run LCA
+#8 run LCA
 LCAmodel <- poLCA(f, dataset3, nclass=2, nrep=100, graphs=TRUE)
 
-# Factor the variables
+#9 Factor the variables
 dataset3 <- as.data.frame(lapply(dataset3, as.factor))
 
-# Assigning the latent class to the dataset
+#10 Assigning the latent class to the dataset
 dataset3$LCA.Class <- factor(as.character(LCAmodel$predclass))
 
-# conduct logistic regression
+#11 conduct logistic regression
 model <- glm(LCA.Class ~ BHSSA02A+BHSSA03A+BHSSC02+BHSSCFail+BHSF02+BHSF05+
               BHSSF02+BHSSF04
               +BHSSX01+BHSSX06
               +BHSSX05+BHSSA05+
               BHSSA06+BHSSA07+BHSSA08+BHST01+BHST02+BHST03+BHST04, data=dataset3, family="binomial")
- summary(model)
+summary(model)
 
 # this model includes the top 7 from the paper; we have more managable beta coefficients here
 # when we lower the number of predictors in the logistic regression model
